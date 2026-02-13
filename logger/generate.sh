562003b487940cc2df9f4b41a2513b1c0d9af630
Mika Nokka <mika.nokka1@gmail.com>
2026-02-13 20:17:06 +0200

    Initial commit

---
+++ logger/generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LOG_FILE=${LOG_FILE:-/logs/app.log}
+INTERVAL=${INTERVAL:-1}
+
+mkdir -p "$(dirname "$LOG_FILE")"
+
+echo "Starting fake log generator"
+echo "Writing to $LOG_FILE"
+
+while true; do
+    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    LEVELS=("INFO" "WARN" "ERROR" "DEBUG")
+    LEVEL=${LEVELS[$RANDOM % 4]}
+    MESSAGE="Test message number $RANDOM"
+
+    echo "$TIMESTAMP [$LEVEL] $MESSAGE" | tee -a "$LOG_FILE"
+    sleep "$INTERVAL"
+done
